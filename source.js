54d0e0c262119cae533b1e110a2151e458618b8a
Mislah <76743829+mislah@users.noreply.github.com>
2021-12-24 20:09:32 +0530

    Create source.js

---
+++ source.js
@@ -0,0 +1,21 @@
+//Send the message n times if #n# is at the end of the message
+document.addEventListener('click', () => {
+    document.querySelectorAll("[contenteditable='true']")[1].addEventListener("keydown", event => {
+        if(event.keyCode == 13){
+            var msgBox = document.querySelectorAll("[contenteditable='true']")[1];
+            msgBox.innerHTML = msgBox.innerHTML.replace('<br>','');
+            if(msgBox.innerHTML.search(/#\d+#$/)!=-1){
+                event.preventDefault();
+                const type = new UIEvent('input', {bubbles: true, cancelable: true, view: window});
+                const enter = new KeyboardEvent('keydown', {keyCode: 13, bubbles: true, view: window});
+                var [message, count] = msgBox.innerHTML.split(/#(\d+)#$/);
+                count = parseInt(count);
+                while(count--) {
+                    msgBox.innerHTML = message;
+                    msgBox.dispatchEvent(type);
+                    msgBox.dispatchEvent(enter);
+                }
+            }
+        }
+    });
+});
